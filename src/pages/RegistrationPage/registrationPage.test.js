d6c35aeaf26e813c364e337d6900f057875fe288
Stepan Snigur <snigur-stepan@mail.ru>
2021-03-18 19:24:01 +0300

    Add new tests

--- src/pages/RegistrationPage/registrationPage.test.js
+++ src/pages/RegistrationPage/registrationPage.test.js
@@ -1,7 +1,40 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 import RegistrationPage from './RegistrationPage'
 
 describe('Registration page', () => {
+  const mockStore = configureStore()
+
   it('Correct render', () => {
-    
+    const initialState = {
+      registrationPage: {
+        errors: null
+      }
+    }
+    const store = mockStore(initialState)
+    render(
+      <Provider store={store}>
+        <RegistrationPage />
+      </Provider>
+    )
+    expect(screen.getByText('Projecto')).toBeInTheDocument()
+  })
+  it('Correct render errors', () => {
+    const errorMessage = 'error message'
+    const initialState = {
+      registrationPage: {
+        errors: [errorMessage]
+      }
+    }
+    const store = mockStore(initialState)
+    render(
+      <Provider store={store}>
+        <RegistrationPage />
+      </Provider>
+    )
+    expect(screen.getByText(errorMessage)).toBeInTheDocument()
   })
 })
