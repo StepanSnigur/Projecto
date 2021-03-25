b8599b1ba9d7a4f4c274fa39e1bee3c68c9935b4
Stepan Snigur <snigur-stepan@mail.ru>
2021-03-25 12:07:52 +0300

    Add progress bar to App page

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AppRouter from './Router'
 import ErrorManager from '../features/ErrorManager'
+import ProgressBar from '../features/ProgressBar'
 
 const App = () => {
   return (
     <>
       <ErrorManager />
       <AppRouter />
+      <ProgressBar />
     </>
   )
 }
