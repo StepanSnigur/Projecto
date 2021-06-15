b8a1c9ccf599ff728339837a44c868455d94d9ee
Stepan Snigur <snigur-stepan@mail.ru>
2021-06-15 18:25:14 +0300

    Remove control buttons when user not logged in

--- src/pages/BoardPage/utils.ts
+++ src/pages/BoardPage/utils.ts
@@ -1,7 +1,12 @@
+import { ITableMember } from '../../features/AddNewTable/AddNewTable'
+
 export const isHexColor = (str: string) => /^#/.test(str)
 export const moveToPosition = (list: any[], deleteFrom: number, insertTo: number): any[] => {
   let listToChange = [...list]
   const itemToMove = listToChange.splice(deleteFrom, 1)[0]
   listToChange.splice(insertTo, 0, itemToMove)
   return listToChange
+}
+export const checkIsBoardMember = (members: ITableMember[], userId: string) => {
+  return members.some(member => member._id === userId)
 }
